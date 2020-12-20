9850ce0074e531c478a8fe50e801bc2eb0f86362
Jean Lukas Andrade Restelli <jeanlarestelli@gmail.com>
2020-12-20 11:22:18 -0300

    tentando criar novas funçoes como mudar mapa e icones

--- script.js
+++ script.js
@@ -1,146 +1,181 @@
-let timerId = null //armazena chamadas da funçao timeout
+let timerId = null; //armazena chamadas da funçao timeout
 
 function boasVindas(){
-alert('Seja Bem Vindo ao meu joguinho, clique no botão verde para proseguir')
+alert('Seja Bem Vindo ao meu joguinho, clique no botão verde para proseguir');
 }
 
 function iniciaGame(){
-var nivel_game = document.getElementById('nivel_game').value
+var nivel_game = document.getElementById('nivel_game').value;
     
-    window.location.href = 'game.html?' + nivel_game
+    window.location.href = 'game.html?' + nivel_game;
 }
 
 function IniciaGame(){
 
-   var url = window.location.search
+   var url = window.location.search;
 
-   var nivelGame = url.replace("?", "")
+   var nivelGame = url.replace("?", "");
 
-   var tempoSegundos = 0
+   var tempoSegundos = 0;
 
-   let quantidadeDeJs = 0
+   let quantidadeDeJs = 0;
 
    if(nivelGame == 1){//1 nivel 240 segundos + 48 js
         
-    tempoSegundos = 240
-    quantidadeDeJs = 48
+    tempoSegundos = 240;
+    quantidadeDeJs = 48;
    }
 
    if(nivelGame == 2){//2 nivel 120 segundos + 35 js
-    tempoSegundos = 120
-    quantidadeDeJs = 35
+    tempoSegundos = 120;
+    quantidadeDeJs = 35;
    }
    
    if(nivelGame == 3){//3 nivel 60  segundos + 24 js
-    tempoSegundos = 60
-    quantidadeDeJs = 24
+    tempoSegundos = 60;
+    quantidadeDeJs = 24;
    }
 
    if(nivelGame == 4){//4 nivel 30  segundos + 14 js
-    tempoSegundos = 30
-    quantidadeDeJs = 14
+    tempoSegundos = 30;
+    quantidadeDeJs = 14;
    }
 
    if(nivelGame == 5){//5 nivel 15  segundos + 7 js
-    tempoSegundos = 15
-     quantidadeDeJs = 7
+    tempoSegundos = 15;
+     quantidadeDeJs = 7;
    }
 
    //inserindo os segundos
-   document.getElementById('cronometro').innerHTML = tempoSegundos
+   document.getElementById('cronometro').innerHTML = tempoSegundos;
    
    //quantidade de baloes
-   let quantidadeJs = quantidadeDeJs
+   let quantidadeJs = quantidadeDeJs;
    
-   criaJs(quantidadeJs)
+   criaJs(quantidadeJs);
 
    //cria as quantidades
-   document.getElementById('js_inteiros').innerHTML = quantidadeJs
-   document.getElementById('js_estourados').innerHTML = 0
+   document.getElementById('js_inteiros').innerHTML = quantidadeJs;
+   document.getElementById('js_estourados').innerHTML = 0;
 
-   contagemTempo(tempoSegundos + 1)
+   contagemTempo(tempoSegundos + 1);
 
    }
    
    function contagemTempo(segundos){
 
-        segundos = segundos - 1
+        segundos = segundos - 1;
 
         if(segundos == -1) {
-            clearTimeout(timerId) //para o tempo
-            gameOver()
-            return false
+            clearTimeout(timerId); //para o tempo
+            gameOver();
+            return false;
         }
 
     document.getElementById('cronometro').innerHTML = segundos;
 
-    timerId = setTimeout("contagemTempo("+segundos+")", 1000)
+    timerId = setTimeout("contagemTempo("+segundos+")", 1000);
 
    }
 
 function gameOver(){
     
-    alert('Fim de jogo, você perdeu.')
+    alert('Fim de jogo, você perdeu.');
 
-    removeEventoEstourar()
+    removeEventoEstourar();
 
-}situacaoJogo
+}situacaoJogo;
 
 function criaJs(quantidadeJs){
 
         for( let i = 1; i <= quantidadeJs; i++) {
 
-            let javascript = document.createElement("img")
-            javascript.src = 'imagens/jssmall1.png'
-            javascript.style.margin = '10px'
-            javascript.id = 'b' + i
+            let javascript = document.createElement("img");
+            javascript.src = 'imagens/jssmall1.png';
+            javascript.style.margin = '10px';
+            javascript.id = 'b' + i;
             javascript.onclick = function(){ estourar(this) }
-            document.getElementById('fundo').appendChild(javascript)
+            document.getElementById('fundo').appendChild(javascript);
         }
 }
 
 function estourar(e) {
-    let idJs = e.id
+    let idJs = e.id;
 
-    document.getElementById(idJs).setAttribute("onclick", "")
-    document.getElementById(idJs).src = 'imagens/jssmallestourado.png'
+    document.getElementById(idJs).setAttribute("onclick", "");
+    document.getElementById(idJs).src = 'imagens/jssmallestourado.png';
     
-    pontuacao(-1)
+    pontuacao(-1);
 }
 
 function pontuacao(acao){
-    var jsInteiros = document.getElementById('js_inteiros').innerHTML
-    var jsEstourados = document.getElementById('js_estourados').innerHTML
+    var jsInteiros = document.getElementById('js_inteiros').innerHTML;
+    var jsEstourados = document.getElementById('js_estourados').innerHTML;
     
-    jsInteiros = parseInt(jsInteiros)
-    jsEstourados = parseInt(jsEstourados)
+    jsInteiros = parseInt(jsInteiros);
+    jsEstourados = parseInt(jsEstourados);
 
-    jsInteiros = jsInteiros + acao
-    jsEstourados = jsEstourados - acao
+    jsInteiros = jsInteiros + acao;
+    jsEstourados = jsEstourados - acao;
 
-    document.getElementById('js_inteiros').innerHTML = jsInteiros
-    document.getElementById('js_estourados').innerHTML = jsEstourados
-    
-    situacaoJogo(jsInteiros)
+    document.getElementById('js_inteiros').innerHTML = jsInteiros;
+    document.getElementById('js_estourados').innerHTML = jsEstourados;
+
+    situacaoJogo(jsInteiros);
 }
 function situacaoJogo(jsInteiros){
     if(jsInteiros == 0){
-        alert('Parabéns, você venceu')
-        paraJogo()
+        alert('Parabéns, você venceu');
+        paraJogo();
+        
     }
 }
 
 function removeEventoEstourar(){
 
-    var i = 1 
+    var i = 1 ;
 
     while(document.getElementById('b' + 1)){
        
-        document.getElementById('b' + 1).onclick = ''
-        i++   
+        document.getElementById('b' + 1).onclick = '';
+        i++   ;
     }
 }
+/* criar funçao para mudar cenario no nivel 3
+ function mudaCenario(){
+     if(nivelGame == 3){
+         
+     }
+ }
+ */
+
+ /*muda simbolo para html no 3
+ function mudaFigura(){
+     if(nivelGame == 3){
+
+     }
+ }
+ */
+/* criar funçao para mudar cenario no nivel 5
+ function mudaCenario1(){
+     if(nivelGame == 5){
+         
+     }
+ }
+ */
+
+ /*muda simbolo para js no 5
+ function mudaFigura1(){
+     if(nivelGame == 5){
+
+     }
+ }
+ */
+function voltaOpt() {
+    window.history.go(-1);
+     return false;
+    }
 
 function paraJogo() {
-    clearTimeout(timerId)
+    clearTimeout(timerId);
 }
